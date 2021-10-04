8922d7dfeb0c6a64d6ba04fe5229c0fb4b655e12
sebastian <sebastian.n.schmidt@gmail.com>
2021-10-04 16:39:17 +0200

    add first reactive modul

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ init vue project
 ```
 vue create hello-world
 ```
+
+
+
+
+
+tutorial: https://medium.com/js-dojo/getting-started-with-vuejs-for-web-and-native-285dc64f0f0d
--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :msg="message"/>
+    <input type="text" v-model="message" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
+  name: 'app',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      message: 'Hello World!'
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+// styles omitted
